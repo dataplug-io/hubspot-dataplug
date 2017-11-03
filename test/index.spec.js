54582b886266324a526d8d47122cd01e356c8d58
Alexey Pelykh <alexey.pelykh@gmail.com>
2017-11-03 07:32:44 +0200

    Added test suites

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,23 @@
 /* eslint-env node, mocha */
 require('chai')
   .should()
+const dataplugTestsuite = require('@dataplug/dataplug-testsuite')
+const hubspotDataplug = require('../lib')
+
+describe('hubspot-dataplug', () => {
+  dataplugTestsuite
+    .forCollection('companies', hubspotDataplug.companies)
+    .use()
+
+  dataplugTestsuite
+    .forCollection('contacts', hubspotDataplug.contacts)
+    .use()
+
+  dataplugTestsuite
+    .forCollection('deals', hubspotDataplug.deals)
+    .use()
+
+  dataplugTestsuite
+    .forCollection('owners', hubspotDataplug.owners)
+    .use()
+})
